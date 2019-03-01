189fc439cc53cf54be8cf7d8141edf0be26cf9a9
Joseph Mena <jmenasih@everis.com>
2019-03-01 15:01:43 -0500

    style(notas): se ordeno para mejor visualizacion. feat(completable): se agrego un test

---
+++ java/rxjava/2.x/src/main/java/org/zer0/pocs/reactivex/rxjava/TestCompletable.java
@@ -0,0 +1,21 @@
+package org.zer0.pocs.reactivex.rxjava;
+
+import io.reactivex.Completable;
+
+public class TestCompletable {
+
+	// Completabe no emite items, sino solo una señal de que se completo una actividad o error, 
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private void testCompetable1() {
+		Completable c = Completable.complete();
+		
+	}
+	
+	
+	
+	
+}
